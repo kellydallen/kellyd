ca4062e10e83f0fbba3aa2ad44411e94de93829c
David Allen Kelly <kellydallen@gmail.com>
2025-03-18 19:57:48 -0400

    title styling and other experimentation

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://kellyd.com";
+export const SITE_TITLE = "kelly, d(avid)";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "The personal and professional site of David Allen Kelly, aka Kelly D.";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "kellyd@kellyd.com";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
       fontFamily: {
         sans: ["Geist", ...defaultTheme.fontFamily.sans],
         mono: ["'Geist Mono'", ...defaultTheme.fontFamily.mono],
+        heading: ["Milligram", "sans-serif"],
       },
     },
   },
